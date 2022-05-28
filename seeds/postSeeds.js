21799bb6c5c2206a2e0a841af5d87a0eb33c3aa3
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-28 18:15:11 +1000

    added seeds for posts

---
+++ seeds/postSeeds.js
@@ -0,0 +1,33 @@
+const { Post } = require("../models");
+
+const postdata = [
+	{
+		title: "Lorem ipsum",
+		post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		user_id: 1,
+	},
+	{
+		title: "Curabitur at mi pharetra",
+		post_text: "Curabitur at mi pharetra, suscipit orci ultrices, aliquam justo.",
+		user_id: 2,
+	},
+	{
+		title: "Phasellus ultricies",
+		post_text: "Phasellus ultricies sollicitudin nulla imperdiet facilisis.",
+		user_id: 3,
+	},
+	{
+		title: "Vivamus venenatis",
+		post_text: "Vivamus venenatis et justo id bibendum.",
+		user_id: 4,
+	},
+	{
+		title: "Donec scelerisque",
+		post_text: "Donec scelerisque ullamcorper hendrerit.",
+		user_id: 5,
+	},
+];
+
+const seedPost = () => Painting.bulkCreate(postdata);
+
+module.exports = seedPost;
