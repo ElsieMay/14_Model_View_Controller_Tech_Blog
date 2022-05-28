f526b0f54dee485a9288a5116dd62ad166e58892
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-28 20:24:30 +1000

    fixed bug in models

--- seeds/indexSeeds.js
+++ seeds/indexSeeds.js
@@ -7,14 +7,14 @@ const seedAll = async () => {
 	await sequelize.sync({ force: true });
 	console.log("\n----- DATABASE SYNCED -----\n");
 
-	await commentSeeds();
-	console.log("\n----- SEEDED COMMENTS -----\n");
+	await userSeeds();
+	console.log("\n----- SEEDED USERS -----\n");
 
 	await postSeeds();
 	console.log("\n----- SEEDED POSTS -----\n");
 
-	await userSeeds();
-	console.log("\n----- SEEDED USERS -----\n");
+	await commentSeeds();
+	console.log("\n----- SEEDED COMMENTS -----\n");
 
 	process.exit(0);
 };
