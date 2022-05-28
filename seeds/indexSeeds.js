9a8dd79cb33c71ae8dedaa9ecdb5c5add2b46446
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-28 17:59:08 +1000

    added seeds index.js

--- seeds/indexSeeds.js
+++ seeds/indexSeeds.js
@@ -1,25 +1,22 @@
 const sequelize = require("../config/connection");
-const { User, Project } = require("../models");
+const commentSeeds = require("./commentSeeds");
+const postSeeds = require("./postSeeds");
+const userSeeds = require("./userSeeds");
 
-const userData = require("./userData.json");
-const projectData = require("./projectData.json");
-
-const seedDatabase = async () => {
+const seedAll = async () => {
 	await sequelize.sync({ force: true });
+	console.log("\n----- DATABASE SYNCED -----\n");
 
-	const users = await User.bulkCreate(userData, {
-		individualHooks: true,
-		returning: true,
-	});
+	await commentSeeds();
+	console.log("\n----- SEEDED COMMENTS -----\n");
 
-	for (const project of projectData) {
-		await Project.create({
-			...project,
-			user_id: users[Math.floor(Math.random() * users.length)].id,
-		});
-	}
+	await postSeeds();
+	console.log("\n----- SEEDED POSTS -----\n");
+
+	await userSeeds();
+	console.log("\n----- SEEDED USERS -----\n");
 
 	process.exit(0);
 };
 
-seedDatabase();
+seedAll();
