bd9d55b86e1722324e324898091fdc8c69a18441
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-28 15:22:47 +1000

    added all relationships to model index.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,32 @@ const Comment = require("./comment");
 const Post = require("./post");
 const User = require("./user");
 
-User.hasMany(Project, {
+Comment.belongsto(User, {
 	foreignKey: "user_id",
-	onDelete: "CASCADE",
+	onDelete: "SET NULL",
+});
+
+Comment.belongsto(Post, {
+	foreignKey: "post_id",
+	onDelete: "SET NULL",
 });
 
 Post.belongsto(User, {
 	foreignKey: "user_id",
 	onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+	foreignKey: "post_id",
+});
+
+User.hasMany(Post, {
+	foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+	foreignKey: "user_id",
+	onDelete: "CASCADE",
 });
 
 module.exports = { Comment, Post, User };
