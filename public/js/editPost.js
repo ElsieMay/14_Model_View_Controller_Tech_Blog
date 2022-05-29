93da47953a7b7f5b58811192c7d12ce9a1c46bff
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-29 12:09:54 +1000

    added edit post function

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,42 +1,24 @@
-const newFormHandler = async (event) => {
+const editFormHandler = async (event) => {
 	event.preventDefault();
 
-	const name = document.querySelector("#post-name").value.trim();
+	const title = document.querySelector("#post-title").value.trim();
 	const description = document.querySelector("#post-desc").value.trim();
 
-	if (name && needed_funding && description) {
-		const response = await fetch(`/api/posts`, {
-			method: "POST",
-			body: JSON.stringify({ name, description }),
+	if (title && description) {
+		const response = await fetch(`/api/posts/${id}`, {
+			method: "PUT",
+			body: JSON.stringify({ title, description }),
 			headers: {
 				"Content-Type": "application/json",
 			},
 		});
 
 		if (response.ok) {
-			document.location.replace("/post");
+			document.location.replace("/dashboard");
 		} else {
 			alert("Failed to create post");
 		}
 	}
 };
 
-const delButtonHandler = async (event) => {
-	if (event.target.hasAttribute("data-id")) {
-		const id = event.target.getAttribute("data-id");
-
-		const response = await fetch(`/api/posts/${id}`, {
-			method: "DELETE",
-		});
-
-		if (response.ok) {
-			document.location.replace("/post");
-		} else {
-			alert("Failed to delete post");
-		}
-	}
-};
-
-document.querySelector(".new-post-form").addEventListener("submit", newFormHandler);
-
-document.querySelector(".post-list").addEventListener("click", delButtonHandler);
+document.querySelector(".edit-post-form").addEventListener("submit", editFormHandler);
