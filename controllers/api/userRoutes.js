a231b97e56bfdd752e06eb93e098796977b0b088
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-29 10:57:19 +1000

    added get all for user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+	try {
+		const userData = await Project.findAll({
+			attributes: { exclude: ["password"] },
+		});
+
+		// Serialize data so the template can read it
+		const users = userData.map((users) => users.get({ plain: true }));
+
+		// Pass serialized data and session flag into template
+		res.render("users", {
+			users,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 router.post("/", async (req, res) => {
 	try {
 		const userData = await User.create(req.body);
