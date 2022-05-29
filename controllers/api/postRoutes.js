6c5984f08844291f1b11ddda38bef12d09998ea7
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-29 11:18:14 +1000

    added put request to update posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -74,6 +74,28 @@ router.post("/", withAuth, async (req, res) => {
 	}
 });
 
+router.put("/:id", withAuth, async (req, res) => {
+	try {
+		const postData = await Post.update(req.params.id, {
+			include: [
+				{
+					title: req.body.title,
+					post_text: req.body.post_text,
+				},
+			],
+		});
+
+		if (!postData) {
+			res.status(404).json({ message: "No post found with this id!" });
+			return;
+		}
+
+		res.status(200).json(postData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 router.delete("/:id", withAuth, async (req, res) => {
 	try {
 		const projectData = await Project.destroy({
