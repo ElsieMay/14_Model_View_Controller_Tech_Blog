99b4ddfc53300b4d2c3c3dabda93cb83147d1cde
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-29 12:04:15 +1000

    added event handler to make comment in comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const commentFormHandler = async (event) => {
+	event.preventDefault();
+
+	const comment = document.querySelector("#comment-body").value.trim();
+
+	if (comment) {
+		const response = await fetch(`/api/comments`, {
+			method: "POST",
+			body: JSON.stringify({ post_id, comment }),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		if (response.ok) {
+			document.location.reload();
+		} else {
+			alert("Failed to create comment");
+		}
+	}
+};
+
+document.querySelector(".comment-form").addEventListener("submit", commentFormHandler);
