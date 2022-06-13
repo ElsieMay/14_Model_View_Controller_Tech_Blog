f1d5ca7956b1a3e1913d313c9e4ab8bf4d43f85a
ElsieMay <elsiemaylawrie1@gmail.com>
2022-06-13 11:36:10 +1000

    .

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -77,20 +77,15 @@ router.post("/", withAuth, async (req, res) => {
 
 router.put("/:id", withAuth, async (req, res) => {
 	try {
-		const postData = await Post.update(req.params.id, {
-			include: [
-				{
-					title: req.body.title,
-					post_text: req.body.post_text,
-				},
-			],
+		const postData = await Post.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
 		});
-
 		if (!postData) {
 			res.status(404).json({ message: "No post found with this id!" });
 			return;
 		}
-
 		res.status(200).json(postData);
 	} catch (err) {
 		console.error(err);
--- public/js/comment.js
+++ public/js/comment.js
@@ -6,7 +6,7 @@ const commentFormHandler = async (event) => {
 	const post_id = window.location.toString().split("/")[window.location.toString().split("/").length - 1];
 
 	if (comment_text) {
-		const response = await fetch(`/api/comments/`, {
+		const response = await fetch(`/api/comments`, {
 			method: "POST",
 			body: JSON.stringify({ post_id, comment_text }),
 			headers: {
